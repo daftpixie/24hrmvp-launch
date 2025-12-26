081c456668f7815919fbbab41704a261a600537e
MattAdams <matthew@beatsthatmatter.com>
2025-12-26 18:27:23 -0500

    fix: next.config build error

--- next.config.js
+++ next.config.js
@@ -112,6 +112,8 @@ const nextConfig = {
     NEXT_PUBLIC_PUNKS_URL: process.env.NEXT_PUBLIC_PUNKS_URL || 'https://punks.24hrmvp.xyz',
     NEXT_PUBLIC_MAIN_URL: process.env.NEXT_PUBLIC_MAIN_URL || 'https://24hrmvp.xyz',
     NEXT_PUBLIC_LAUNCH_DATE: process.env.NEXT_PUBLIC_LAUNCH_DATE || '2025-12-26T10:00:00Z',
+    // Google Sheets webhook for beta signups
+    NEXT_PUBLIC_GSHEET_WEBHOOK_URL: process.env.NEXT_PUBLIC_GSHEET_WEBHOOK_URL || '',
   },
 };
 
